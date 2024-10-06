ecdd54aaba0c4bd5523fab86a37c3a60a7045e4d
shivangisaraf <157287050+shivangisaraf@users.noreply.github.com>
2024-10-06 23:00:03 +0530

    Update Binary Search.java

--- Algorithms/Binary Search.java
+++ Algorithms/Binary Search.java
@@ -1,47 +1,30 @@
 // Java implementation of iterative Binary Search
 
-import java.io.*;
+package DSA_JAVA;
 
-class BinarySearch {
-  
-    // Returns index of x if it is present in arr[].
-    int binarySearch(int arr[], int x)
-    {
-        int low = 0, high = arr.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+public class binarySearchAlgo {
+    public static void main(String[]args){
+    int[]arr={23,45,67,89,94};
+    int target=67;
+    int ans=search(arr,target);
+    System.out.print("index:"+ans);
+}
+static int search(int[]arr,int target){
+       int start=0;
+       int end=arr.length-1;
+       while(start<=end){
+           int mid=start+(end-start)/2;
+       if(target<arr[mid]){
+           end=mid-1;
+       }
+       else if(target>arr[mid]){
+           start=mid+1;
+       }
+       else{
+           return mid;
+       }
 
-            // Check if x is present at mid
-            if (arr[mid] == x)
-                return mid;
-
-            // If x greater, ignore left half
-            if (arr[mid] < x)
-                low = mid + 1;
-
-            // If x is smaller, ignore right half
-            else
-                high = mid - 1;
-        }
-
-        // If we reach here, then element was
-        // not present
+       }
         return -1;
-    }
-
-    // Driver code
-    public static void main(String args[])
-    {
-        BinarySearch ob = new BinarySearch();
-        int arr[] = { 2, 3, 4, 10, 40 };
-        int n = arr.length;
-        int x = 10;
-        int result = ob.binarySearch(arr, x);
-        if (result == -1)
-            System.out.println(
-                "Element is not present in array");
-        else
-            System.out.println("Element is present at "
-                               + "index " + result);
-    }
+}
 }
