ceedad72dbc01de32ae6bfa4c060cb30933143d5
sanviwaghamare <149017812+sanviwaghamare@users.noreply.github.com>
2024-10-08 17:06:55 +0530

    Added linear search code in cpp

---
+++ linearsearch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            return i; // Return the index where the key is found
+        }
+    }
+    return -1; // Return -1 if the key is not found
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 30;
+
+    int result = linearSearch(arr, size, key);
+
+    if (result != -1) {
+        cout << "Element found at index: " << result << endl;
+    } else {
+        cout << "Element not found!" << endl;
+    }
+
+    return 0;
+}
