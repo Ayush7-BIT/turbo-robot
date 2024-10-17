f94c8f19a2fbabcaccb3e138b8bf0fd82a432a36
Rohan Rathod <87760317+Rohan-Rathod@users.noreply.github.com>
2024-10-17 12:32:24 +0530

    Addition program in JS

---
+++ Algorithms/Addition.js
@@ -0,0 +1,11 @@
+// Function to add two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Example usage
+let number1 = 5;
+let number2 = 10;
+
+let result = addNumbers(number1, number2);
+console.log("The sum is: " + result);
