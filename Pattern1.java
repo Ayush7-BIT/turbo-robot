19698f86343cfa87575951e0404ffc7de84b7338
Itachi <148188238+Harinath333@users.noreply.github.com>
2024-10-17 23:25:57 +0530

    Added simple easy pattern code in java!!

---
+++ Pattern1.java
@@ -0,0 +1,15 @@
+class Pattern1
+  {
+    public static void main (String[] args)
+    {
+      for(int row=1;row<=5;row++)
+        {
+          for(int col=1;col<=row;col++)
+            {
+              System.out.print("*");
+            }
+          System.out.println();
+        }
+    }
+
+  }
